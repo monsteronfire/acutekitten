f3c1e892a2b6c2282615478757313d4d60f1970e
monsteronfire <monsteronfire@gmail.com>
2015-08-12 16:00:18 +0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acutekittenApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the acutekittenApp
+ */
+angular.module('acutekittenApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acutekittenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the acutekittenApp
+ */
+angular.module('acutekittenApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
