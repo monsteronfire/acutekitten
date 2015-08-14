e93b031da6556cde0955999610c72d411e997fd5
monsteronfire <monsteronfire@gmail.com>
2015-08-14 15:54:18 +0800

    removes ng-upload-file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,10 @@
  * # MainCtrl
  * Controller of the acutekittenApp
  */
-angular.module('acutekittenApp')
-  .controller('MainCtrl', function ($scope) {
+
+var App = App || angular.module('acutekittenApp', []);
+
+App.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
